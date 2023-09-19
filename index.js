9c4712940a8724a018f0181aacd3e05321f10cf1
Yahin Jose M V <84230561+Yahin@users.noreply.github.com>
2023-09-19 15:59:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 for( var i =0 ; i< document.querySelectorAll(".drum").length; i++){
 document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    alert("myr oombi");
+    alert("yeay");
     }  );
 }
      
