8c8966649c7eeabd4d20a4ab0cac485f0f105345
Yahin Jose M V <84230561+Yahin@users.noreply.github.com>
2023-09-19 16:41:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 for( var i =0 ; i< document.querySelectorAll(".drum").length; i++){
 document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    alert("yeay");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
     }  );
 }
-     
